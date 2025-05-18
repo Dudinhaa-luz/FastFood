47339cab290e0f5011d244e0ba3904f657677453
Givaldo Alencar <givaldo.alencar@gmail.com>
2025-05-18 16:42:01 -0300

    Mapeamento da entidade Cliente

--- FastFood.Infrastructure/AppDbContext.cs
+++ FastFood.Infrastructure/AppDbContext.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FastFood.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FastFood.Infrastructure
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext()
-        {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public DbSet<Client> Clients => Set<Client>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.ToTable("clientes");
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.Name).IsRequired();
+                entity.Property(u => u.CPF).IsRequired();
+                entity.Property(u => u.Email).IsRequired();
+                entity.Property(u => u.Phone).IsRequired();
+            });
         }
     }
 }
