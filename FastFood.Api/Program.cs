120d8d55352b190daac2ccf0aac2ed78e7d82683
Givaldo Alencar <givaldo.alencar@gmail.com>
2025-05-18 16:46:36 -0300

    Revisão de escopo e configuração para execução de migrations pendentes

--- FastFood.Api/Program.cs
+++ FastFood.Api/Program.cs
@@ -3,9 +3,11 @@ using FastFood.Application.Mappings;
 using FastFood.Application.Services;
 using FastFood.Application.Validators;
 using FastFood.Domain.Interfaces.Repositories;
+using FastFood.Infrastructure;
 using FastFood.Infrastructure.Repositories;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +17,24 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddAutoMapper(typeof(ClientProfile));
 
-builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IClientService, ClientService>();
+
+
+
 
 builder.Services.AddValidatorsFromAssemblyContaining<CreateClientRequestValidator>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate(); // aplica as migrations pendentes
+}
 
 if (app.Environment.IsDevelopment())
 {
